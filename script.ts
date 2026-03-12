e40838f3aec60c5e74b96e87bd7130aebbe0c25e
4ndreLuis <4ndredev21@gmail.com>
2026-03-12 11:37:55 -0300

    feat: mapeia elementos do DOM e inicializa array de logs

---
+++ script.ts
@@ -0,0 +1,14 @@
+const inputUser = document.querySelector("#inputUser") as HTMLInputElement;
+
+const btnGrant = document.querySelector("#btnGrant") as HTMLButtonElement;
+
+const logList = document.querySelector("#logList") as HTMLUListElement;
+
+const totalCount = document.querySelector("#totalCount") as HTMLSpanElement;
+
+let logs: string[] = [];
+let contador: number = 0;
+
+const atualizarEstatisticas = () => {
+  totalCount.textContent = contador.toString();
+};
